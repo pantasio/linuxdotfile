3162281dc705c1a93f28992e548530de4e7644a0
pantasio <3162016+pantasio@users.noreply.github.com>
2023-06-05 13:00:19 +0700

    Feat(wezterm) init

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,80 +1,134 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
-local wsl_domains = wezterm.default_wsl_domains()
+-- local launch_menu = {}
+-- local default_shell = "/bin/zsh"
+local padding = {
+	left = "1cell",
+	right = "1cell",
+	top = "0.5cell",
+	bottom = "0.5cell",
+}
 
-wezterm.on("gui-startup", function(cmd)
-  local _, _, window = wezterm.mux.spawn_window(cmd or {})
-  window:gui_window():toggle_fullscreen()
+-- Reload the configuration every ten minutes
+wezterm.time.call_after(600, function()
+	wezterm.reload_configuration()
 end)
 
-for _, domain in ipairs(wsl_domains) do
-  domain.default_cwd = "~"
+-- A helper function for my fallback fonts
+local function font_with_fallback(name, params)
+	local names = { name, "mini-file-icons", "Hack Nerd Font", "SauceCodePro Nerd Font" }
+	return wezterm.font_with_fallback(names, params)
+end
+
+local function get_theme()
+	local _time = os.date("*t")
+	if _time.hour >= 1 and _time.hour < 9 then
+		return "Rosé Pine (base16)"
+	elseif _time.hour >= 9 and _time.hour < 17 then
+		return "tokyonight_night"
+	elseif _time.hour >= 17 and _time.hour < 21 then
+		return "Catppuccin Mocha"
+	elseif _time.hour >= 21 and _time.hour < 24 or _time.hour >= 0 and _time.hour < 1 then
+		return "kanagawabones"
+	end
 end
 
 return {
-  adjust_window_size_when_changing_font_size = false,
-  audible_bell = 'Disabled',
-  background = {
-    {
-      source = {
-        -- File = 'd:\\btsync\\shared\\ricki\\illustrations\\ricki-do-not-delete-this-one.png',
-        File = 'D:\\BTSync\\agilenet\\nodes\\common\\WezTerm\\themes\\catpuccin\\frappe\\background.png',
-        -- File = 'D:\\BTSync\\agilenet\\nodes\\common\\WezTerm\\themes\\catpuccin\\mocha\\background.png',
-      },
-    },
-    -- {
-    --   source = {
-    --     -- File = 'D:\\BTSync\\shared\\ricki\\Illustrations\\Illustration_02_1920x1080.png',
-    --     -- File = 'D:\\BTSync\\shared\\ricki\\Illustrations\\Illustration_02_1920x1080_v02.png',
-    --     -- File = 'D:\\BTSync\\shared\\ricki\\Illustrations\\Illustration_02_3840x2160_v02.png',
-    --     -- File = 'D:\\BTSync\\shared\\ricki\\i4.png',
-    --     -- File = 'D:\\BTSync\\shared\\ricki\\i5.png',
-    --     -- File = 'D:\\test.png',
-    --     File = 'D:\\elephant.png',
-    --   },
-    --   horizontal_align = 'Center',
-    --   vertical_align = 'Middle',
-    --   height = 'Contain',
-    --   width = 'Contain',
-    --   repeat_x = 'NoRepeat',
-    --   repeat_y = 'NoRepeat',
-    --   opacity = 0.1
-    -- },
-  },
-  color_scheme = 'Catppuccin Frappe',
-  disable_default_key_bindings = true,
-  exit_behavior = 'Close',
-  font = wezterm.font {
-    family = 'FiraCode Nerd Font Mono',
-    weight = 'Light',
-    stretch = 'Normal',
-    style = 'Normal',
-    harfbuzz_features = { 'cv29', 'cv30', 'ss01', 'ss03', 'ss06', 'ss07', 'ss09' },
-  },
-  font_size = 20,
-  force_reverse_video_cursor = true,
-  hide_mouse_cursor_when_typing = true,
-  hide_tab_bar_if_only_one_tab = true,
-  keys = {
-    { action = wezterm.action.ActivateCommandPalette, mods = 'CTRL|SHIFT', key =     'P' },
-    { action = wezterm.action.CopyTo    'Clipboard' , mods = 'CTRL|SHIFT', key =     'C' },
-    { action = wezterm.action.DecreaseFontSize      , mods =       'CTRL', key =     '-' },
-    { action = wezterm.action.IncreaseFontSize      , mods =       'CTRL', key =     '=' },
-    { action = wezterm.action.Nop                   , mods =        'ALT', key = 'Enter' },
-    { action = wezterm.action.PasteFrom 'Clipboard' , mods = 'CTRL|SHIFT', key =     'V' },
-    { action = wezterm.action.ResetFontSize         , mods =       'CTRL', key =     '0' },
-    { action = wezterm.action.ToggleFullScreen      ,                      key =   'F11' },
-  },
-  scrollback_lines = 10000,
-  show_update_window = true,
-  use_dead_keys = false,
-  unicode_version = 15,
-  window_close_confirmation = 'NeverPrompt',
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = '0.6cell',
-    bottom = 0,
-  },
-  wsl_domains = wsl_domains,
+	bidi_enabled = true,
+	bidi_direction = "AutoLeftToRight",
+	color_scheme = get_theme(),
+	font = font_with_fallback({
+		-- family = "FiraCode Nerd Font",
+		family = "SFMono Nerd Font Mono",
+		harfbuzz_features = {
+			"zero",
+		},
+	}),
+	font_rules = {
+		{
+			intensity = "Bold",
+			font = font_with_fallback({
+				family = "FiraCode Nerd Font",
+				harfbuzz_features = {
+					"zero",
+				},
+				weight = "Medium",
+			}),
+		},
+		{
+			italic = true,
+			intensity = "Bold",
+			font = font_with_fallback({
+				family = "Iosevka Nerd Font",
+				-- family = "Dank Mono",
+				weight = "Medium",
+				italic = true,
+			}),
+		},
+		{
+			italic = true,
+			font = font_with_fallback({
+				-- family = "Dank Mono",
+				family = "Iosevka Nerd Font",
+				weight = "Regular",
+				italic = true,
+			}),
+		},
+	},
+	-- leader = { key = "a", mods = "CTRL" },
+	-- keys = {
+	-- 	-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+	-- 	{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
+	-- 	{ key = "\"", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	-- 	{ key = "%", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	-- 	{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+	-- 	{ key = "t", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+	-- 	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	-- 	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	-- 	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	-- 	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+	-- 	{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+	-- 	{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+	-- 	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+	-- 	{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+	-- 	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+	-- 	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+	-- 	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+	-- 	{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+	-- 	{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+	-- 	{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+	-- 	{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+	-- 	{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+	-- 	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+	-- 	{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+	-- 	{ key = "w", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+	-- 	-- { key = "z", mods = "LEADER", action = "ToggleFullScreen" },
+	-- },
+	initial_cols = 128,
+	initial_rows = 32,
+	use_dead_keys = false,
+	window_padding = padding,
+	window_decorations = "RESIZE",
+	hide_tab_bar_if_only_one_tab = true,
+	selection_word_boundary = " \t\n{}[]()\"'`,;:@",
+	-- disable_default_key_bindings = true,
+	line_height = 1.30,
+	font_size = 13,
+	window_background_opacity = 0.85,
+	bold_brightens_ansi_colors = false,
+	-- swap_backspace_and_delete = false,
+	-- term = "wezterm",
+	-- freetype_load_target = "Light",
+	
+	-- unicode_version = 15,
+	-- window_close_confirmation = 'NeverPrompt',
+  	-- window_padding = {
+    -- 	left = 0,
+    -- 	right = 0,
+    -- 	top = '0.6cell',
+    -- 	bottom = 0,
+  	-- },
+  	-- wsl_domains = wsl_domains,
+
+
 }
