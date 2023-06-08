925041b1dd8f44b6398e1299b7953383f73b3526
pantasio <3162016+pantasio@users.noreply.github.com>
2023-06-09 05:43:48 +0700

    feat(luasnippets): Add lua snippets

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -69,6 +69,39 @@ local plugins = {
     end,
   },
 
+  -- Override nvterm config
+  {
+    "NvChad/nvterm",
+    config = function ()
+      require("nvterm").setup({
+        terminals = {
+          shell = vim.o.shell,
+          list = {},
+          type_opts = {
+            float = {
+              relative = 'editor',
+              row = 0.68,
+              col = 0.68,
+              width = 0.7,
+              height = 0.5,
+              border = "single",
+            },
+            horizontal = { location = "rightbelow", split_ratio = .39, },
+            vertical = { location = "rightbelow", split_ratio = .5 },
+          }
+        },
+        behavior = {
+          autoclose_on_quit = {
+            enabled = false,
+            confirm = true,
+          },
+          close_on_exit = true,
+          auto_insert = true,
+        },
+      })
+    end,
+  }
+
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
